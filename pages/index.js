af240a563a3f21ce60189af759c2c13ab921c934
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-02-27 23:05:39 +0700

    Feat: weather app

--- pages/index.js
+++ pages/index.js
@@ -8,39 +8,71 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            temperature: 18,
-            lon: 0,
-            lat: 0,
+            latitude: 19.796,
+            longitude: 105.775,
+            temp: 18,
             degree: 'C',
-            city: 'Ha Noi',
+            city: 'Thanh Hoa',
             country: 'VN',
-            weather: '',
-            weatherImgUrl: ''
+            weather: 'Clouds',
+            iconUrl: 'https://cdn.freecodecamp.org/weather-icons/04n.png'
         }
+        this.setWeatherDataToState = this.setWeatherDataToState.bind(this)
+        this.changeDegree = this.changeDegree.bind(this)
+        this.newWeather = this.newWeather.bind(this)
+        this.getCurrentWeather = this.getCurrentWeather.bind(this)
     }
 
-    component
-
     componentDidMount() {
+        this.getWeatherData(this.state.latitude, this.state.longitude)
+            .then(data => {
+                this.setWeatherDataToState(data)
+            })
+    }
+
+    async getWeatherData(latitude, longitude) {
+        let url = 'https://weather-proxy.freecodecamp.rocks/api/current?lat=' + latitude + '&lon=' + longitude
+        let res = await fetch(url)
+        return await res.json()
+    }
+
+    setWeatherDataToState(data) {
+        this.setState({
+            latitude: data.coord.lat,
+            longitude: data.coord.lon,
+            temp: Math.round(data.main.temp),
+            city: data.name || '',
+            country: data.sys.country || '',
+            weather: data.weather[0].main,
+            iconUrl: data.weather[0].icon,
+            degree: 'C'
+        })
+    }
+
+    changeDegree() {
+        this.setState(state => ({
+            degree: state.degree == 'C' ? 'F' : 'C',
+            temp: state.degree == 'F' ? Math.round(state.temp * 9 / 5 + 32) : Math.round((state.temp - 32) * 5 / 9)
+        }))
+    }
+
+    newWeather(e) {
+        e.preventDefault();
+        this.getWeatherData(this.state.latitude, this.state.longitude)
+            .then(data => {
+                this.setWeatherDataToState(data)
+            })
+    }
+
+    getCurrentWeather(e) {
+        e.preventDefault();
         window.navigator.geolocation.getCurrentPosition(position => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude;
-            let url = 'https://weather-proxy.freecodecamp.rocks/api/current?lat=' + lat + '&lon=' + lon
-            fetch(url).then(res => res.json())
+            this.getWeatherData(position.coords.latitude, position.coords.longitude)
                 .then(data => {
-                    this.setState({
-                        lon: lon,
-                        lat: lat,
-                        temperature: data.main.temp,
-                        city: data.name,
-                        country: data.sys.country,
-                        weather: data.weather[0].main,
-                        weatherImgUrl: data.weather[0].icon
-                    })
+                    this.setWeatherDataToState(data)
                 })
-                .catch(err => {
-                    console.log(err)
-                })
+        }, (err) => {
+            alert('Cannot get your current location!')
         })
     }
 
@@ -48,16 +80,21 @@ class Home extends React.Component {
         return (
             <>
                 <Head>
-                    <title>Weather App</title>
+                    <title>Weather</title>
                 </Head>
                 <main className={styles.home}>
                     <h1>Weather App</h1>
-                    <p>{this.state.lat}</p>
-                    <p>{this.state.lon}</p>
-                    <p>{this.state.temperature} {this.state.degree}</p>
-                    <p>{this.state.city}, {this.state.country}</p>
-                    <p>{this.state.weather}</p>
-                    <p><img src={this.state.weatherImgUrl} /></p>
+                    <p className={styles.temp}>{this.state.temp} &deg;<button onClick={this.changeDegree} className={styles.degree}>{this.state.degree}</button></p>
+                    <p className={styles.weather}>{this.state.weather}</p>
+                    <p className={styles.icon}><img src={this.state.iconUrl} /></p>
+                    <p className={styles.city}>{this.state.city == '' ? 'Unknow' : this.state.city} city</p>
+                    <p className={styles.country}>{this.state.country == '' ? 'Unknow' : this.state.country} country</p>
+                    <form>
+                        <input type='number' placeholder='latitude' value={this.state.latitude} step={0.1} onChange={(e) => this.setState({ latitude: e.target.value })} />
+                        <input type='number' placeholder='longitude' value={this.state.longitude} step={0.1} onChange={(e) => this.setState({ longitude: e.target.value })} /> <br />
+                        <input type='submit' value='Get Weather with latitude and longitude' onClick={this.newWeather} /> <br />
+                        or <button onClick={this.getCurrentWeather}>Get Weather in your current location</button>
+                    </form>
                 </main>
             </>
         )

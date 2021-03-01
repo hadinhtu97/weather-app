b6d4c3bb0f2b29061295fe2ae7a0d15fb7cbe583
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-01 21:10:08 +0700

    Refactor: delete useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import fetch from 'node-fetch'
 
